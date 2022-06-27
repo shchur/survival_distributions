0392dfade1eec3c6ee203a8d6a7f00c333ec7d2b
Oleksandr Shchur <oleksandr.shchur@tum.de>
2022-06-27 12:03:56 +0200

    Validate args for Logistic and Normal

--- survival_distributions/logistic.py
+++ survival_distributions/logistic.py
@@ -16,22 +16,32 @@ class Logistic(SurvivalDistribution):
         super().__init__(batch_shape, validate_args=validate_args)
 
     def sf(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
         z = (value - self.loc) / self.scale
         return torch.sigmoid(-z)
 
     def logsf(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
         z = (value - self.loc) / self.scale
         return F.logsigmoid(-z)
 
     def cdf(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
         z = (value - self.loc) / self.scale
         return torch.sigmoid(z)
 
     def log_prob(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
         z = (value - self.loc) / self.scale
         return z - self.scale.log() - 2 * F.softplus(z)
 
     def isf(self, u):
+        if self._validate_args:
+            assert constraints.unit_interval.check(u).all()
         z = (-u).log1p() - u.log()
         return self.scale * z + self.loc
 
--- survival_distributions/normal.py
+++ survival_distributions/normal.py
@@ -1,12 +1,15 @@
 import math
 
 import torch
+from torch.distributions import constraints
 
 from .survival_distribution import SurvivalDistribution
 
 
 class Normal(torch.distributions.Normal, SurvivalDistribution):
     def sf(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
         z = (value - self.loc) / self.scale
         return 1.0 - torch.special.ndtr(z)
 
@@ -14,10 +17,14 @@ class Normal(torch.distributions.Normal, SurvivalDistribution):
         return torch.log(self.sf(value))
 
     def logcdf(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
         z = (value - self.loc) / self.scale
         return torch.log(torch.special.ndtr(z))
 
     def isf(self, u):
+        if self._validate_args:
+            assert constraints.unit_interval.check(u).all()
         return self.loc + self.scale * torch.erfinv(1 - 2 * u) * math.sqrt(2)
 
     def _new_tensor(self, shape):
