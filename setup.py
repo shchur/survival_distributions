b120de68673234100faf3027914c4091653ba702
Oleksandr Shchur <oleksandr.shchur@tum.de>
2022-06-27 19:06:10 +0200

    Add PyPI release files

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="survival_distributions",
     author="Oleksandr Shchur",
-    version="0.0.1",
+    description="Extended functionality for univariate probability distributions in PyTorch",
+    version="0.0.2",
+    license="MIT",
     author_email="oleks.shchur@gmail.com",
+    url="https://github.com/shchur/survival_distributions",
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
+    install_requires=["torch>=1.10"],
 )
